d1eef78b20e15ebac6b6530ebb5baca45aa2c545
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-08-01 23:26:11 +0200

    Added dictionary api

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const userInput = document.querySelector("#userInput");
+const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 const searchBtn = document.querySelector("#searchBtn");
 
-searchBtn.addEventListener("click", function () {
-  console.log(userInput.value);
+searchBtn.addEventListener("click", () => {
+  let inputWord = document.querySelector("#userInput").value;
+  fetch(`${url}${inputWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 });
