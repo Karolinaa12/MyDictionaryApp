c6772cd2178b98ef98086ca91d104c3b5ce802dd
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-08-01 23:30:12 +0200

    Created input word display

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 const searchBtn = document.querySelector("#searchBtn");
+const result = document.querySelector(".result");
 
 searchBtn.addEventListener("click", () => {
   let inputWord = document.querySelector("#userInput").value;
@@ -7,5 +8,7 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      result.innerHTML = `<div class="word">
+      <h1>${inputWord}</h1>`;
     });
 });
