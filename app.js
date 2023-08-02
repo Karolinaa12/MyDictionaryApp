df4c279a8ad1297795237605a238c42f875d46f0
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-08-02 16:26:49 +0200

    Iterating word meanings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 const searchBtn = document.querySelector("#searchBtn");
 const result = document.querySelector(".result");
+const header = document.querySelector(".header");
 
 searchBtn.addEventListener("click", () => {
   let inputWord = document.querySelector("#userInput").value;
@@ -8,30 +9,27 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      result.innerHTML = `<div class="word">
-      <h1>${inputWord}</h1>
-      </div>
-      <div class="details">
-      <p>${data[0].phonetic || data[0].phonetics[1].text}
-      <i class="part-of-speech">${data[0].meanings[0].partOfSpeech}</i></p>
-      </div>
-      <p class="word-meaning">
-      ${data[0].meanings[0].definitions[0].definition}
-      </p>
-      <p class="word-example">
-      ${data[0].meanings[0].definitions[0].example || ""}
-      </p>
-      <br />
+      let meaningsNo = data[0].meanings.length;
+      for (let i = 0; i < meaningsNo; i++) {
+        console.log(data[0].meanings[i].definitions[0].definition);
+        header.innerHTML = data[0].meanings[i].definitions[0].definition;
 
-      <div class="details1">
-      <p>${data[0].phonetic || data[0].phonetics[1].text}
-      <i class="part-of-speech">${data[0].meanings[1].partOfSpeech}</i></p>
-      </div>
-      <p class="word-meaning1">
-      ${data[0].meanings[1].definitions[0].definition}
-      </p>
-      <p class="word-example2">
-      ${data[0].meanings[1].definitions[0].example || ""}
-      </p>`;
+        let meaning = data[0].meanings[i].definitions[0].definition;
+        result.innerHTML = `<div class="word">
+        <h1>${inputWord}</h1>
+        </div>
+        <div class="details">
+        <p>${data[0].phonetic || data[0].phonetics[1].text}
+        <i class="part-of-speech">${data[0].meanings[i].partOfSpeech}</i></p>
+        </div>
+        <p class="word-meaning">
+        ${meaning}
+        // ${data[0].meanings[i].definitions[0].definition}
+        </p>
+        <p class="word-example">
+        ${data[0].meanings[i].definitions[0].example || ""}
+        </p>
+        <br />`;
+      }
     });
 });
