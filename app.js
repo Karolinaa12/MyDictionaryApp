63a118e6b8abae32a543cd1df52a775d05355858
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-08-02 15:53:52 +0200

    Added word meaning and example display

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ searchBtn.addEventListener("click", () => {
     .then((data) => {
       console.log(data);
       result.innerHTML = `<div class="word">
-      <h1>${inputWord}</h1>`;
+      <h1>${inputWord}</h1>
+      </div>
+      <div class="details">
+      <p>${data[0].phonetic || data[0].phonetics[1].text}
+      <i class="part-of-speech">${data[0].meanings[0].partOfSpeech}</i></p>
+      </div>
+      <p class="word-meaning">
+      ${data[0].meanings[0].definitions[0].definition}
+      </p>
+      <p class="word-example">
+      ${data[0].meanings[0].definitions[0].example || ""}
+      </p>`;
     });
 });
